4ffde36ed1d7f00a0ee8223cc78530e7196e2eda
lokeshbs1994 <lokeshbs619@gmail.com>
2022-03-24 18:37:28 +0530

    Added: Creating Employee Payroll service to read from console and write to console

---
+++ src/main/java/com/bridgelabz/EmployeePayRollService.java
@@ -0,0 +1,30 @@
+package com.bridgelabz;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class EmployeePayRollService {
+    Scanner scanner = new Scanner(System.in);
+    ArrayList<EmployeePayroll> employeePayrolls = new ArrayList<EmployeePayroll>();
+
+    public static void main(String[] args) {
+        EmployeePayRollService service = new EmployeePayRollService();
+        service.readData();
+        service.writeData();
+    }
+
+    private void readData() {
+        EmployeePayroll employeePayroll = new EmployeePayroll();
+        System.out.println("Enter Id");
+        employeePayroll.setId(scanner.nextInt());
+        System.out.println("Enter Name");
+        employeePayroll.setName(scanner.next());
+        System.out.println("Enter Salary");
+        employeePayroll.setSalary(scanner.nextInt());
+        employeePayrolls.add(employeePayroll);
+    }
+
+    private void writeData() {
+        System.out.println(employeePayrolls);
+    }
+}
